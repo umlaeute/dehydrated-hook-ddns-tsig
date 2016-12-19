8338f6a4d75759a9f615ccaf3ffd6451e326b28c
IOhannes m zmölnig <zmoelnig@iem.at>
2016-12-19 09:37:23 +0100

    cron-snippet that uses 'chronic' (from moreutils)

---
+++ debian/dehydrated-dnspython-hook.cron.d
@@ -0,0 +1,12 @@
+# /etc/cron.d/dehydrated: crontab entries for dehydrated
+#
+# certbot recommends attempting renewal twice a day. we do the same
+#
+# Eventually, this will be an opportunity to validate certificates
+# haven't been revoked, etc.  Renewal will only occur if expiration
+# is within 30 days.
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+0 */12 * * * root perl -e 'sleep int(rand(3600))' && chronic /usr/bin/dehydrated -c
+
